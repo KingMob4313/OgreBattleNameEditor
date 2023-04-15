a68325eefe3e635909609450e046e47691da6543
Frederick Bell <shortstop4313@gmail.com>
2023-04-15 17:18:13 -0400

    Add project files.

---
+++ OgreBattleNameEditor/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace OgreBattleNameEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.FileName = "Super Nintendo Rom"; // Default file name
+            dialog.DefaultExt = ".smc"; // Default file extension
+            dialog.Filter = "SMC ROM(.smc)|*.smc"; // Filter files by extension
+
+            // Show open file dialog box
+            bool? result = dialog.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                string fileName = dialog.FileName;
+                byte[] fileBytes = File.ReadAllBytes(fileName);
+
+            }
+        }
+    }
+}
---
+++ OgreBattleNameEditor/NameProcessor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OgreBattleNameEditor
+{
+    class NameProcessor
+    {
+        //Will name this smarter later
+        string prefixedHex = "0x00088d3c";
+
+        public List<NameRecord> processByteRecords(byte[] fileBytes)
+        {
+            int nameCount = 0;
+            nameCount = getRecordCount(fileBytes);
+        }
+
+        public List<NameRecord> processNameRecords(byte[] fileBytes)
+        {
+
+        }
+
+        //Working quickly should be a smarter way that won't involve reading the file multiple times
+        public int getRecordCount(byte[] fileBytes)
+        {
+            int startByte = Convert.ToInt32(prefixedHex, 16);
+            var endByte = startByte + readEndByte(fileBytes, startByte);
+
+
+
+        }
+        
+        public int readEndByte(byte[] fileBytes, int intByteStartValue)
+        {
+            int byteLocationCounter = intByteStartValue;
+            while (0 != fileBytes[intByteStartValue])
+            {
+                byteLocationCounter++;
+            }
+            return (byteLocationCounter - 1);
+        }
+
+    }
+}
