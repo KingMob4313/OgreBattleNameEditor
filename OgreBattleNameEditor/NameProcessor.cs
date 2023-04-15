c8d729edc5175308c63ae3072a87fa7338fec64f
Frederick Bell <shortstop4313@gmail.com>
2023-04-15 19:38:38 -0400

    Bulk of Code

--- OgreBattleNameEditor/NameProcessor.cs
+++ OgreBattleNameEditor/NameProcessor.cs
@@ -13,29 +13,67 @@ namespace OgreBattleNameEditor
 
         public List<NameRecord> processByteRecords(byte[] fileBytes)
         {
+            List<NameRecord> currentNameRecords = new List<NameRecord>();
+
             int nameCount = 0;
             nameCount = getRecordCount(fileBytes);
+            return currentNameRecords;
         }
 
         public List<NameRecord> processNameRecords(byte[] fileBytes)
         {
+            int byteStart = ;
+            int recordCount = getRecordCount(fileBytes, byteStart);
+            List<NameRecord> collectedNameRecords = new List<NameRecord>();
+            int byteLocation = byteStart;
 
+            for (int i = 0; i < recordCount; i++)
+            {
+                NameRecord currentNameRecord = new NameRecord();
+                currentNameRecord.NameByteStart = byteStart;
+                currentNameRecord.NameLength = (readEndByte(fileBytes, byteStart)) - byteStart;
+                currentNameRecord.NameId = i;
+                currentNameRecord.NameStartInHex = byteStart.ToString("X");
+                currentNameRecord.OldName = getNameFromBytes(fileBytes, byteStart, currentNameRecord.NameLength);
+            }
+        }
+
+        private string getNameFromBytes(byte[] fileBytes, int byteStart, int nameLength)
+        {
+            string currentName = string.Empty;
+            List<byte> nameByte = new List<byte>();
+            for (int i = 0; i < nameLength; i++)
+            {
+                nameByte.Add(fileBytes[byteStart + i]);
+            }
+            currentName = System.Text.Encoding.Default.GetString(nameByte.ToArray());
+            return currentName;
         }
 
         //Working quickly should be a smarter way that won't involve reading the file multiple times
-        public int getRecordCount(byte[] fileBytes)
+        public int getRecordCount(byte[] fileBytes, int byteLocationStart)
         {
-            int startByte = Convert.ToInt32(prefixedHex, 16);
-            var endByte = startByte + readEndByte(fileBytes, startByte);
-
-
-
+            int recordCount = 0;
+            int startByte = byteLocationStart;
+            int endByte = 0; 
+            bool areRecordsDone = false;
+            while (areRecordsDone)
+            {
+                endByte = startByte + readEndByte(fileBytes, startByte);
+                startByte = startByte + endByte;
+                areRecordsDone = startByte == endByte;
+                if (!areRecordsDone)
+                {
+                    recordCount++;
+                }
+            }
+            return recordCount;
         }
         
         public int readEndByte(byte[] fileBytes, int intByteStartValue)
         {
             int byteLocationCounter = intByteStartValue;
-            while (0 != fileBytes[intByteStartValue])
+            while (fileBytes[intByteStartValue] != 0)
             {
                 byteLocationCounter++;
             }
--- OgreBattleNameEditor/NameRecord.cs
+++ OgreBattleNameEditor/NameRecord.cs
@@ -8,10 +8,20 @@ namespace OgreBattleNameEditor
 {
     class NameRecord
     {
-        int nameByteStart;
-        int nameLength;
+        private int nameByteStart;
+        private int nameLength;
 
-        string oldName;
-        string newName;
+        private int nameId;
+        private string nameStartInHex;
+
+        private string oldName;
+        private string newName;
+
+        public int NameByteStart { get => nameByteStart; set => nameByteStart = value; }
+        public int NameLength { get => nameLength; set => nameLength = value; }
+        public string OldName { get => oldName; set => oldName = value; }
+        public string NewName { get => newName; set => newName = value; }
+        public int NameId { get => nameId; set => nameId = value; }
+        public string NameStartInHex { get => nameStartInHex; set => nameStartInHex = value; }
     }
 }
